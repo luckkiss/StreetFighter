393263b5e7b2e5b255bee76284877497c0fce78e
setsuodu <339131001@qq.com>
2019-10-15 01:10:28 +0800

    移除角色prefab的碰撞体

--- bin/index.js
+++ bin/index.js
@@ -11,6 +11,6 @@ window.screenOrientation = "sensor_landscape";
 loadLib("libs/laya.core.js")
 loadLib("libs/laya.ui.js")
 loadLib("libs/laya.d3.js")
-loadLib("libs/laya.physics3D.js")
+loadLib("libs/laya.physics.js")
 //-----libs-end-------
 loadLib("js/bundle.js");
